e9dd07ad224ff3125cc8373356c49a218bdfd5c1
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-01-28 19:01:49 +0000

    Added hiCLIP preprocessing script

---
+++ linker/preprocess_hiclip.sh
@@ -0,0 +1,49 @@
+# Preprocess data from Sugimoto et al., 2015
+# A. M. Chakrabarti
+# 12th June 2020, updated 16th September 2020, updated 28th January 2021
+
+conda activate comp-hiclip-dev
+
+cd /camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/preprocessed
+
+DATADIR=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/data/hiclip_stau1
+GITHUBDIR=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/comp-hiclip
+
+# Move UMI to header and demultiplex
+umi_tools extract -p NNNXXXXNN -I $DATADIR/ERR605257.fastq.gz -S ERR605257.umi.fastq.gz
+reformat.sh trimreaddescription=t in=ERR605257.umi.fastq.gz out=ERR605257.umi.trd.fastq.gz # Need to remove everything after first whitespace in read name
+cutadapt -j 8 -e 0 --no-indels -g LigPlusLow="^AATA" -g LigPlusHigh="^GGTT" -g LigMinus="^GGCG" -o {name}.fastq.gz ERR605257.umi.trd.fastq.gz > cutadapt.log 2>&1
+
+rm ERR605257.umi.fastq.gz
+rm ERR605257.umi.trd.fastq.gz
+rm LigMinus.fastq.gz
+rm unknown.fastq.gz
+
+# Trim adapters and identify linker reads
+A=AGATCGGAAGAGCGGTTCAG
+B=CTGTAGGCACCATACAATG
+
+for i in LigPlusHigh.fastq.gz LigPlusLow.fastq.gz; do
+
+    echo ${i%%.*}
+
+    # Trim 3' BA concatemers and A
+    cutadapt -j 8 -a $B$A -m 12 -n 10 -o ${i%%.*}.BA_trimmed.fastq.gz $i > ${i%%.*}_BA.cutadapt.log
+    cutadapt -j 8 -a $A -m 12 -n 10 -o ${i%%.*}.A_BA_trimmed.fastq.gz ${i%%.*}.BA_trimmed.fastq.gz > ${i%%.*}_A_BA.cutadapt.log
+
+    # Identify reads with B in the middle and at least 12 nt arms either side
+    $GITHUBDIR/linker/linker.R ${i%%.*}.A_BA_trimmed.fastq.gz ${i%%.*}.linker.fastq.gz
+
+    # Filter all adapter removed FASTQ for linker read ids
+    zcat ${i%%.*}.linker.fastq.gz | \
+    grep '^@R.' | \
+    sed 's/^@R.//' \
+    > ${i%%.*}.hits.txt
+
+    filterbyname.sh in=${i%%.*}.A_BA_trimmed.fastq.gz out=${i%%.*}.nolinker.fastq.gz names=${i%%.*}.hits.txt include=f
+
+    rm ${i%%.*}.BA_trimmed.fastq.gz
+    rm ${i%%.*}.A_BA_trimmed.fastq.gz
+    rm ${i%%.*}.hits.txt
+
+done
