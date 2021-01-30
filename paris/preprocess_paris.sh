105c032133af42d91d2b7ea536ba0227324682ab
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-01-30 19:43:56 +0000

    Updated PARIS analysis to pre-dedup

--- paris/preprocess_paris.sh
+++ paris/preprocess_paris.sh
@@ -22,12 +22,31 @@ DATADIR=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/data/paris
 # Barcode is in positions 7-10, so adapter is reverse complemented
 # Therefore UMI format will be NNNNNNXXXXNNNNN
 
-#                        543210987654321
-#                        123456789012345
+#                        3210987654321
+#                        1234567890123
 # iCLIP_ddRT_BC1: /5phos/DDDNNAACCNNNNAGATCGGAAGAGCGTCGTGAT/iSp18/GG ATCC/iSp18/TACTGAACCGC
+
+
+# zcat SRR2814763.fastq.gz | awk 'NR % 4 == 2' | cut -c 7-10 | sort | uniq -c | sort -k1nr | head
+# zcat SRR2814763.fastq.gz | awk 'NR % 4 == 2' | cut -c 5-8 | sort | uniq -c | sort -k1nr | head
+
+# for i in SRR2814763 SRR2814764 SRR2814765; do
+
+#     sbatch -t 12:00:00 -c 8 -o $i.log --wrap="umi_tools extract -p NNNNNNXXXXNNNNN -I $DATADIR/$i.fastq.gz -S $i.umi.fastq.gz && \
+#                                               cutadapt -j 8 -m 12 -u 4 -a AGATCGGAAGAGC -o $i.preprocessed.fastq.gz $i.umi.fastq.gz"
+
+# done
+
+# ==========
+# Pre-collapse
+# ==========
 
 for i in SRR2814763 SRR2814764 SRR2814765; do
 
-    sbatch -t 12:00:00 -c 8 -o $i.log --wrap="umi_tools extract -p NNNNNNXXXXNNNNN -I $DATADIR/$i.fastq.gz -S $i.umi.fastq.gz && cutadapt -j 8 -m 12 -u 4 -a AGATCGGAAGAGC -o $i.preprocessed.fastq.gz $i.umi.fastq.gz"
+   sbatch -t 12:00:00 -c 8 -o $i.log --wrap="\
+    cutadapt -j 8 -m 31 -a AGATCGGAAGAGC -o $i.trimmed.fastq $DATADIR/$i.fastq.gz && \
+    ../comp-hiclip/paris/bin/readCollapse.pl -U $i.trimmed.fastq && \
+    cutadapt -j 8 -u 15 -m 16 -o $i.dedup.fastq.gz $i.trimmed.collapsed.fastq  && \
+    rm $i.trimmed.fastq $i.trimmed.collapsed.fastq $i.trimmed.collapsed.fa"
 
 done
--- paris/run_paris.sh
+++ paris/run_paris.sh
@@ -14,15 +14,16 @@ ml Nextflow/20.10.0
 ml Singularity/3.6.4
 ml Graphviz/2.38.0-foss-2016b
 
-nextflow pull amchakra/tosca -r star_options
+nextflow pull amchakra/tosca
 
-nextflow run amchakra/tosca -r star_options \
+nextflow run amchakra/tosca \
 -resume \
 -profile crick,conda \
--N anob.chakrabarti@crick.ac.uk \
 --org comp_hiclip \
 --input paris.csv \
---outdir /camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/paris \
+--outdir /camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/paris/results \
+--split_size 1000000 \
 --umi_separator _ \
---star_args '--limitOutSJcollapsed 5000000' \
---split_size 1000000
+--dedup_method none
+
+
