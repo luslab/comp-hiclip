b667c5c4abe5aa1ad28728c4ffb2790135cd0b80
Ira Iosub <ira.iosub@gmail.com>
2021-02-23 18:12:32 +0000

    Update map_hg19_to_hg38.R

--- functional_integration/map_hg19_to_hg38.R
+++ functional_integration/map_hg19_to_hg38.R
@@ -14,8 +14,6 @@ library(rio)
 data_list <- import_list("41594_2017_BFnsmb3325_MOESM3_ESM.xlsx", which = c("3b","3c","3d"))
 
 data.df <- data_list %>% reduce(inner_join, by = c("Gene", "Simple"))
-all.genes.ls <- data.df$Gene
-all.genes.ls <- str_remove(all.genes.ls, "\\.[^.]*$") # remove ensembl id suffix
 
 # hg19 annotation from (Mukherjee et al. 2017)
 hg19.gr <- import.gff2("gencode.v19.chr_patch_hapl_scaff.annotation.gtf.gz")
@@ -28,39 +26,39 @@ regions.df <- as.data.frame(regions.gr)
 
 
 # ==========
-# Mapping
+# Mapping: based on matching ensembl IDs and gene names
 # ==========
 
-# Start mapping: based on matching ensembl IDs and gene names
+hg19.df <- semi_join(hg19.df, data.df, by = c("gene_id" = "Gene")) # filter hg19 for genes present in the metabolic data
+
 hg19.map <- hg19.df %>%
-  filter(str_detect(gene_id, paste(all.genes.ls, collapse = "|"))) %>% # filter hg19 for genes present in the metabolic data
   select(gene_id, gene_name) %>%
-  mutate(gene_id_trim = str_remove(gene_id, "\\.[^.]*$"),
-         gene_name_trim = str_remove(gene_name, "\\.[^.]*$"))
+  mutate(gene_id_trimmed = str_remove(gene_id, "\\.[^.]*$"),
+         gene_name_trimmed = str_remove(gene_name, "\\.[^.]*$"))
 
 hg38.map <- regions.df %>% 
   select(gene_id, gene_name) %>%
-  mutate(gene_id_trim = str_remove(gene_id, "\\.[^.]*$"),
-         gene_name_trim = str_remove(gene_name, "\\.[^.]*$"))
+  mutate(gene_id_trimmed = str_remove(gene_id, "\\.[^.]*$"),
+         gene_name_trimmed = str_remove(gene_name, "\\.[^.]*$"))
 
 # matched by gene_id
-hg19to38map_id.df <- inner_join(hg19.map, hg38.map, by = "gene_id_trim", suffix = c(".hg19", ".hg38"))
-hg19to38map_id.df <- distinct(hg19to38map_id.df)
+hg19to38map_id.df <- inner_join(hg19.map, hg38.map, by = "gene_id_trimmed", suffix = c(".hg19", ".hg38")) %>%
+  distinct()
 
 
-# matched by gene_name
+# matched by gene_name (for some genes the gene name is more stable between assemblies than the ensembl id)
 
 # antijoin to match by name only the unmapped hg19 ids
 hg19.map_name <- anti_join(hg19.map, hg19to38map_id.df, by = c("gene_id" = "gene_id.hg19"))
 
-hg19to38map_name.df <- inner_join(hg19.map_name, hg38.map, by = "gene_name_trim", suffix = c(".hg19", ".hg38"))
-hg19to38map_name.df <- distinct(hg19to38map_name.df)
+hg19to38map_name.df <- inner_join(hg19.map_name, hg38.map, by = "gene_name_trimmed", suffix = c(".hg19", ".hg38")) %>%
+  distinct()
 
 hg19to38map_id.df <- hg19to38map_id.df %>%
-  select(-gene_id_trim, -gene_name_trim.hg19, -gene_name_trim.hg38)
+  select(-gene_id_trimmed, -gene_name_trimmed.hg19, -gene_name_trimmed.hg38)
 
 hg19to38map_name.df <- hg19to38map_name.df %>%
-  select(-gene_id_trim.hg19, -gene_name_trim, -gene_id_trim.hg38)
+  select(-gene_id_trimmed.hg19, -gene_name_trimmed, -gene_id_trimmed.hg38)
 
 hg19to38map.df <- rbind(hg19to38map_id.df, hg19to38map_name.df)
 
