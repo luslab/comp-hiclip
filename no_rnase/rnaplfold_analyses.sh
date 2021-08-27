533ba6182163f94418ac207021cc8fd7a5ab7b82
Ira Iosub <ira.iosub@gmail.com>
2021-08-27 16:30:01 +0100

    max TPM downstream changes

--- no_rnase/rnaplfold_analyses.sh
+++ no_rnase/rnaplfold_analyses.sh
@@ -9,10 +9,10 @@ RNASEQ_DIR=$RESULTSDIR/rnaseq/results_demux/salmon
 cd $RESULTSDIR
 
 # Get highest TPM transcript per gene
-#./get_max_tpm_transcripts.R --gtf=$REFDIR/gencode.v33.annotation.gtf.gz --tpm=$RNASEQ_DIR/salmon.merged.transcript_tpm.tsv --out=$RESULTSDIR/gencode.v33_max_tpm_transcripts.bed.gz
+./get_max_tpm_transcripts.R --gtf=$REFDIR/gencode.v33.annotation.gtf.gz --tpm=$RNASEQ_DIR/salmon.merged.transcript_tpm.tsv --out=$RESULTSDIR/gencode.v33_max_tpm_transcripts.bed.gz
 
 # Get longest protein-coding transcripts
-#./get_longest_pcoding_transcripts.R --gtf=$REFDIR/gencode.v33.annotation.gtf.gz --out=$RESULTSDIR/gencode.v33.longest_pcoding_transcripts.tsv.gz
+./get_longest_pcoding_transcripts.R --gtf=$REFDIR/gencode.v33.annotation.gtf.gz --out=$RESULTSDIR/gencode.v33.longest_pcoding_transcripts.tsv.gz
 
 # Annotate peaks with transcript id
 ./annotate_peaks.R --bed=$RESULTSDIR/stau1.10nt_10nt.peaks.bed.gz --txdb=$RESULTSDIR/gencode.v33.txdb.sqlite
@@ -21,5 +21,8 @@ cd $RESULTSDIR
 ./run_rnaplfold.R --bed=$RESULTSDIR/stau1.10nt_10nt.peaks.annot.bed.gz --txdb=$RESULTSDIR/gencode.v33.txdb.sqlite --prefix=$RESULTSDIR/stau1 --shuffle
 
 # Generate RNAplfold unpaired probability metaprofile (-100nt and +100nt window)
-./get_structure_metaprofile.R --bed=$RESULTSDIR/stau1.10nt_10nt.peaks.annot.bed.gz --prob=$RESULTSDIR/stau1_threeutrs.rnaplfold.bed,$RESULTSDIR/stau1_threeutrs.rnaplfold.shuffled.bed --prefix=$RESULTSDIR/stau1
+#./get_structure_metaprofile.R --bed=$RESULTSDIR/stau1.10nt_10nt.peaks.annot.bed.gz --prob=$RESULTSDIR/stau1_threeutrs.rnaplfold.bed.gz,$RESULTSDIR/stau1_threeutrs.rnaplfold.shuffled.bed.gz --prefix=$RESULTSDIR/stau1
+./get_structure_metaprofile.R --bed=$RESULTSDIR/stau1.10nt_10nt.peaks.annot.bed.gz --prob=$RESULTSDIR/stau1_threeutrs.rnaplfold.bed.gz --prefix=$RESULTSDIR/stau1
 
+# Profile for shuffled control
+./get_structure_metaprofile.R --bed=$RESULTSDIR/stau1.10nt_10nt.peaks.annot.bed.gz --prob=$RESULTSDIR/stau1_threeutrs.rnaplfold.shuffled.bed.gz --prefix=$RESULTSDIR/stau1_shuffled
