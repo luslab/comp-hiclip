a0fa0ef0797b1ea909051a51b312f30329376818
Ira Iosub <ira.iosub@gmail.com>
2021-09-20 12:51:55 +0100

    added the non-clustered short-range duplexes to atlas

--- merged_clustered/get_merged_atlas.R
+++ merged_clustered/get_merged_atlas.R
@@ -4,6 +4,8 @@ suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(primavera))
 suppressPackageStartupMessages(library(tictoc))
 suppressPackageStartupMessages(library(parallel))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(dplyr))
 
 # ==========
 # Data
@@ -13,10 +15,12 @@ results.dir <- "/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/m
 
 # Linker
 linker.dt <- fread("/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/results_linker/linker.clusters.mfe.tsv.gz")
+linker.dt$sample <- "stau1_linker"
 
 # No linker
 nolinker.dt <- fread("/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/results_nolinker/atlas/all.hybrids.tsv.gz")
-nolinker.dt[, sample := tstrsplit(sample, "\\.")[[1]]]
+#nolinker.dt[, sample := tstrsplit(sample, "\\.")[[1]]]
+nolinker.dt$sample <- "stau1_nolinker"
 
 # Non-hybrids, files produced in Figure_3.Rmd
 nonhybrid.dt <- fread("/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/results_nonhybrid/lmin/short_range_duplexes.tsv.gz")
@@ -77,28 +81,56 @@ nrow(all.hybrids.dt)
 all.list <- split(all.hybrids.dt, by = c("L_seqnames", "R_seqnames"))
 all.clusters.list <- parallel::mclapply(all.list, cluster_hybrids, percent_overlap = 0.5, mc.cores = 4)
 all.clusters.dt <- rbindlist(all.clusters.list, use.names = TRUE, fill = TRUE)
+all.clusters.dt <- convert_coordinates(all.clusters.dt, genes.gr)
 
 #all.clusters.dt[L_seqnames == R_seqnames][!L_seqnames %in% c("tRNA", "rDNA", "rRNA_5S")][grep("C", cluster), .N, by = .(cluster, L_seqnames, R_seqnames)]
 
 all.collapsed  <- collapse_clusters(all.clusters.dt, mode = "median")
-nrow(all.collapsed)
-
-# ==========
-# Annotate clusters
-# ==========
-
 all.collapsed.dt <- convert_coordinates(all.collapsed, genes.gr)
-all.collapsed.dt <- annotate_hybrids(all.collapsed.dt, regions.gr)
 
-all.clusters.dt <- convert_coordinates(all.clusters.dt, genes.gr)
+# ==========
+# Add the rest of the non-hybrid reads
+# ==========
+
+
+nonhybrid.dt <- fread("/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/results_nonhybrid/lmin/short_range_duplexes.tsv.gz")
+nonhybrid.dt$sample <- "stau1_nonhybrid"
+nonhybrid.dt$cluster <- "stau1_nonhybrid"
+
+test  <-  all.clusters.dt %>%
+  dplyr::filter(sample == "stau1_nonhybrid")
+
+stopifnot(nrow(test) == nrow(nonhybrid.dt))
+
+not.clustered.df <- all.clusters.dt %>%
+  dplyr::filter(sample == "stau1_nonhybrid" & !(str_detect(cluster, "C") ))
+
+nrow(not.clustered.df)
+
+nonhybrid.dt <- nonhybrid.dt %>%
+  dplyr:: filter(!(name %in% not.clustered.df$name)) %>%
+  dplyr::select(colnames(all.collapsed.dt))
+
+nonhybrid.dt <- as.data.table(nonhybrid.dt)
+
+nrow(all.collapsed.dt)
+all.collapsed.dt <- rbind(all.collapsed.dt, nonhybrid.dt)
+nrow(all.collapsed.dt)
+
+# ==========
+# Annotate hybrids and clusters
+# ==========
+
+all.collapsed.dt <- annotate_hybrids(all.collapsed.dt, regions.gr)
 all.clusters.dt <- annotate_hybrids(all.clusters.dt, regions.gr)
+
 
 # ==========
 # Export tables
 # ==========
 
 fwrite(all.clusters.dt, paste0(results.dir,"/merged_atlas.clusters.tsv.gz"), sep = "\t")
-fwrite(all.collapsed.dt, paste0(results.dir,"/merged_atlas.clusters.collapsed.tsv.gz"), sep = "\t")
+fwrite(all.collapsed.dt, paste0(results.dir,"/merged_atlas.clusters.collapsed_plus_nonhybrids.tsv.gz"), sep = "\t")
 
 
 
