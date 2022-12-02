64dffd05195262c2d632633e647dac8ec70fff45
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2022-12-02 16:56:22 +0000

    Updated linker processing

--- linker/map_linker_hybrids.sh
+++ linker/map_linker_hybrids.sh
@@ -3,32 +3,57 @@
 # Map linker data from Sugimoto et al., 2015
 # 01 February 2021
 
-# conda activate comp-hiclip-dev
+conda activate comp-hiclip-dev
 
-cd /camp/lab/luscomben/home/users/iosubi/projects/comp_hiclip/linker
+DATADIR=/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/revisions/preprocessed
+REFDIR=/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/ref
+GITHUBDIR=/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/revisions/comp-hiclip
 
-# DATADIR=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/preprocessed
-WORKDIR=/camp/lab/luscomben/home/users/iosubi/projects/comp_hiclip/linker
+# ==========
+# Generate STAR index
+# ==========
 
+cd $REFDIR
 
-# generate STAR index
+STAR \
+--runThreadN 8 \
+--runMode genomeGenerate \
+--genomeDir STAR_GRCh38_GencodeV33_masked \
+--genomeFastaFiles GRCh38.gencode_v33.fa
 
-# STAR --runMode genomeGenerate --runThreadN 8 --genomeDir STAR_GRCh38_GencodeV33_masked \
-# --genomeFastaFiles /camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/ref/human.fa
+# ==========
 
+mkdir -p /camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/revisions/results_linker
+cd /camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/revisions/results_linker
 
 for i in LigPlusHigh.linker.fastq.gz LigPlusLow.linker.fastq.gz; do
 
     echo ${i%%.*}
 
     # Map
-    STAR --runThreadN 8 --runMode alignReads --genomeDir STAR_GRCh38_GencodeV33_masked \
-	--readFilesIn $i \
-	--readFilesCommand gunzip -c --genomeLoad NoSharedMemory --outFileNamePrefix ${i%%.*} \
-	--outSAMattributes All --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 \
-	--outFilterType BySJout --alignIntronMin 20 --alignIntronMax 100000 --limitBAMsortRAM 60000000000 \
-	--twopassMode Basic --outFilterMultimapNmax 1 --outFilterMultimapScoreRange 1 --outFilterScoreMin 10
-	
+    STAR \
+	--runThreadN 8 \
+	--runMode alignReads \
+	--genomeDir $REFDIR/STAR_GRCh38_GencodeV33_masked \
+	--readFilesIn $DATADIR/$i \
+	--readFilesCommand gunzip -c \
+	--genomeLoad NoSharedMemory \
+	--outFileNamePrefix ${i%%.*}. \
+	--outSAMattributes All \
+	--outSAMtype BAM SortedByCoordinate \
+	--outSAMunmapped Within \
+	--alignSJoverhangMin 8 \
+	--alignSJDBoverhangMin 1 \
+	--outFilterType BySJout \
+	--alignIntronMin 20 \
+	--alignIntronMax 100000 \
+	--limitBAMsortRAM 60000000000 \
+	--twopassMode Basic \
+	--outFilterMultimapNmax 1 \
+	--outFilterMultimapScoreRange 1 \
+	--outFilterScoreMin 10
+
 	# Get hybrid table
-	Rscript --vanilla $WORKDIR/bam_to_dataframe.R ${i%%.*}Aligned.sortedByCoord.out.bam ${i%%.*}
+	Rscript --vanilla $GITHUBDIR/linker/bam_to_dataframe.R ${i%%.*}.Aligned.sortedByCoord.out.bam ${i%%.*}
+
 done
