e230d1d515784519cbfc59cf171a7eebc4973afb
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2022-12-05 13:02:16 +0000

    Updated no linker run script and csv to latest Tosca

--- no_linker/run_nolinker.sh
+++ no_linker/run_nolinker.sh
@@ -3,6 +3,7 @@
 # Process hiCLIP nolinker hybrids
 # A. M. Chakrabarti
 # 28th January 2021
+# 2nd December 2022
 
 #SBATCH --job-name="comp_hiclip"
 #SBATCH --time=24:00:00
@@ -10,102 +11,32 @@
 #SBATCH --output=comp_hiclip-%A.out
 
 ml purge
-ml Nextflow/20.10.0
+ml Nextflow/21.10.3
 ml Singularity/3.6.4
 ml Graphviz/2.38.0-foss-2016b
 
-PROJ=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip
+export NXF_SINGULARITY_CACHEDIR=/camp/lab/luscomben/home/shared/singularity
 
-mkdir -p $PROJ/results_nolinker
-mkdir -p /camp/lab/luscomben/scratch/chakraa2/projects/comp_hiclip
-export NXF_WORK=/camp/lab/luscomben/scratch/chakraa2/projects/comp_hiclip
+REFDIR=/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/ref
+# Regenerate index as comp-hiclip-dev is 2.7.7a and tosca-1.0.0 is 2.7.4a
+# cd $REFDIR && STAR --runMode genomeGenerate --runThreadN 8 --genomeDir STAR_GRCh38_GencodeV33 --genomeFastaFiles GRCh38.gencode_v33.fa 
+GITHUBDIR=/camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/revisions/comp-hiclip
+cd $GITHUBDIR/no_linker
 
-nextflow pull amchakra/tosca
+nextflow pull amchakra/tosca -r v1.0.0
 
-nextflow run amchakra/tosca \
+nextflow run amchakra/tosca -r v1.0.0 \
 -resume \
--profile crick,conda \
---org comp_hiclip \
+-profile crick \
 --input nolinker.csv \
---outdir $PROJ/results_nolinker \
---umi_separator _ \
+--outdir /camp/lab/luscomben/home/shared/projects/ira-nobby/comp_hiclip/revisions/results_nolinker \
+--genome_fai $REFDIR/GRCh38.primary_assembly.genome.fa.fai \
+--star_genome $REFDIR/STAR_GRCh38_GencodeV33 \
+--transcript_fa $REFDIR/GRCh38.gencode_v33.fa \
+--transcript_fai $REFDIR/GRCh38.gencode_v33.fa.fai \
+--transcript_gtf $REFDIR/GRCh38.gencode_v33.tx.gtf.gz \
+--regions_gtf $REFDIR/icount_mini_utr3/regions.gtf.gz \
 --percent_overlap 0.5 \
---analyse_structure true \
+--analyse_structures true \
 --clusters_only true \
---shuffled_mfe true \
---transcript_fa $PROJ/ref/GRCh38.gencode_v33.fa \
---transcript_fai $PROJ/ref/GRCh38.gencode_v33.fa.fai \
---transcript_gtf $PROJ/ref/GRCh38.gencode_v33.tx.gtf.gz
-
-
-nextflow run amchakra/tosca \
--resume \
--profile conda \
---org comp_hiclip \
---input silico.csv \
---outdir $PROJ/results_silico \
---umi_separator _ \
---percent_overlap 0.5 \
---atlas false \
---transcript_fa $PROJ/ref/GRCh38.gencode_v33.fa \
---transcript_fai $PROJ/ref/GRCh38.gencode_v33.fa.fai \
---transcript_gtf $PROJ/ref/GRCh38.gencode_v33.tx.gtf.gz
-
-
-mkdir -p $PROJ/results_nolinker_qc
-
-nextflow pull amchakra/tosca -r multiqc
-
-nextflow run amchakra/tosca -r multiqc \
--resume \
--profile conda \
---org comp_hiclip \
---input nolinker.csv \
---outdir $PROJ/results_nolinker_qc \
---umi_separator _ \
---percent_overlap 0.5 \
---atlas false \
---transcript_fa $PROJ/ref/GRCh38.gencode_v33.fa \
---transcript_fai $PROJ/ref/GRCh38.gencode_v33.fa.fai \
---transcript_gtf $PROJ/ref/GRCh38.gencode_v33.tx.gtf.gz
-
-
-ml purge
-ml Nextflow/20.10.0
-ml Singularity/3.6.4
-ml Graphviz/2.38.0-foss-2016b
-
-PROJ=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip
-
-nextflow run amchakra/tosca -r multiqc \
--resume \
--profile conda \
---org comp_hiclip \
---input silico.csv \
---umi_separator _ \
---percent_overlap 0.5 \
---atlas false \
---transcript_fa $PROJ/ref/GRCh38.gencode_v33.fa \
---transcript_fai $PROJ/ref/GRCh38.gencode_v33.fa.fai \
---transcript_gtf $PROJ/ref/GRCh38.gencode_v33.tx.gtf.gz
-
-
-ml purge
-ml Nextflow/20.10.0
-ml Singularity/3.6.4
-ml Graphviz/2.38.0-foss-2016b
-
-cd /camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip/silico/
-PROJ=/camp/lab/luscomben/home/users/chakraa2/projects/comp_hiclip
-
-nextflow run /camp/home/chakraa2/.nextflow/assets/amchakra/tosca/main.nf \
--resume \
--profile conda \
---org comp_hiclip \
---input silico.csv \
---umi_separator _ \
---percent_overlap 0.5 \
---atlas false \
---transcript_fa $PROJ/ref/GRCh38.gencode_v33.fa \
---transcript_fai $PROJ/ref/GRCh38.gencode_v33.fa.fai \
---transcript_gtf $PROJ/ref/GRCh38.gencode_v33.tx.gtf.gz
+--shuffled_energies true
